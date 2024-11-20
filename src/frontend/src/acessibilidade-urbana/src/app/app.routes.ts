33e9efab1057018da4bc43aff2739da05aecae6d
Luiz <luizgustavoka66@gmail.com>
2024-11-20 11:45:00 -0300

    login

--- src/frontend/src/acessibilidade-urbana/src/app/app.routes.ts
+++ src/frontend/src/acessibilidade-urbana/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/loguin/loguin.component';
+import { LoginComponent } from './features/login/login.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'  
+  },
+  {
     path: 'login',
-    loadComponent: () => import('./features/loguin/loguin.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
   }
 ];
